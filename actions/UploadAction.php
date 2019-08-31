e57b8be7ddf5205582192abbd8f507bd36b44f2a
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2019-08-31 19:23:53 +0400

    Add UploadAction which saves files using fs fly-system component

---
+++ actions/UploadAction.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * User: TheCodeholic
+ * Date: 8/31/2019
+ * Time: 6:23 PM
+ */
+
+namespace thecodeholic\yii2grapesjs\actions;
+
+
+use Yii;
+use yii\web\Response;
+use yii\web\UploadedFile;
+
+/**
+ * Class UploadAction
+ *
+ * @author Zura Sekhniashvili <zurasekhniashvili@gmail.com>
+ * @package thecodeholic\yii2grapesjs\actions
+ */
+class UploadAction extends BaseAction
+{
+    public function runWithParams($params)
+    {
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        $response = [];
+        /**
+         * @var UploadedFile[] $files
+         */
+        $files = UploadedFile::getInstancesByName('files');
+        foreach ($files as $file) {
+            $stream = fopen($file->tempName, 'r+');
+            Yii::$app->fs->writeStream($file->name, $stream);
+            $response[] = '/files/' . $file->name;
+        }
+        return [
+            'data' => $response
+        ];
+    }
+}
